43849dc7a6716dc460c8dda66daf211c8601c139
Dhruv Patel <57500359+Patel-9825@users.noreply.github.com>
2022-12-09 16:07:23 -0500

    Add project files.

---
+++ Models/FinalBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab5NETD.Models
+{
+    public class FinalBook
+    {
+        public int ID { get; set; }
+        public string title { get; set; }
+
+        public int isbn { get; set; }
+
+        public int version { get; set; }
+
+        public int price { get; set; }
+
+        public string condition { get; set; }
+    }
+}
